d2ea6d3e9490aac5cdf09bf95918f92dd5d1651a
Mike <wl02599509@gmail.com>
2024-05-26 22:47:09 +0800

    adjust

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 7 }
   validates :content, presence: true, length: { maximum: 50 }
 end
